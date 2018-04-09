62cceab5e7bb69bd008e73f2f06cd5b488500895
Hannah Hatherell <hath0013@algonquinlive.com>
2018-04-08 20:03:44 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="app">
+    <!-- We need some navigation links to switch between views -->
+    <nav>
+      <ul>
+        <!--
+          The RouterLink component comes with VueRouter.
+          The 'to' prop is the URL route.
+        -->
+        <li><router-link to="/" exact>All</router-link></li>
+        <li><router-link to="/active">Active</router-link></li>
+        <li><router-link to="/completed">Completed</router-link></li>
+      </ul>
+    </nav>
+    <h1 class="app-title">Vue Todos</h1>
+    <add-task-form @taskAdded="addTask" />
+    <!--
+      The RouterView is a placeholder that VueRouter uses to know
+      where to insert the designated component for a given URL.
+      In this case we are passing our taskList as a prop to each route's
+      component and we are listening for user events.  This way we are still
+      only mainting one master list in the App.vue component.
+     -->
+    <router-view
+      :tasks="taskList"
+      @toggleDone="toggleDone"
+      @removeTask="removeTask"
+    />
+  </div>
+</template>
+
+<script>
+import AddTaskForm from '@/components/AddTaskForm'
+
+export default {
+  name: 'App',
+  components: { AddTaskForm },
+  data () {
+    return {
+      taskList: [
+        // { id: 1234, title: 'Learn Vue', isComplete: true, priority: 'medium' },
+        // { id: 1235, title: 'Learn Vue Router', isComplete: false, priority: 'medium' },
+        // { id: 1236, title: 'Learn Vuex', isComplete: false, priority: 'medium' },
+        // { id: 1237, title: 'Learn Vue DevTools', isComplete: true, priority: 'medium' }
+      ]
+    }
+  },
+
+
+  created () {
+    this.taskList = JSON.parse(localStorage.getItem('taskList')) || []
+  },
+
+  methods: {
+    addTask (task) {
+      this.taskList.push(task)
+      this.saveTaskList()
+    },
+
+    toggleDone (task) {
+      task.isComplete = !task.isComplete
+      this.saveTaskList()
+    },
+
+    removeTask (task) {
+      const taskIndex = this.taskList.indexOf(task)
+      this.taskList.splice(taskIndex, 1)
+      this.saveTaskList()
+    },
+
+    saveTaskList () {
+      localStorage.setItem('taskList', JSON.stringify(this.taskList))
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  width: 100%;
+  max-width: 550px;
+  margin: 60px auto;
+}
+
+nav {
+  display: inline-block;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+nav ul {
+  display: flex;
+  justify-content: flex-start;
+}
+nav ul li {
+  margin-right: 1em;
+}
+
+.app-title {
+  display: inline-block;
+  text-align: right;
+  font-weight: normal;
+}
+
+a.router-link-active {
+  color: #01cdd7;
+  border-top: solid 3px #9c3f10;
+  padding-top: .3rem;
+}
+
+.li.router-link-active a {
+  color: #01cdd7;
+  padding-top: .3rem;
+}
+
+a {
+  text-decoration: none;
+  color: #424242;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+
+@media only screen and (min-width: 38rem) {
+
+  .app-title {
+    margin-left: 7rem;
+  }
+
+}
+
+</style>
---
+++ src/components/AddTaskForm.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <label class="block">Title
+      <input type="text" v-model.trim="newTask.title" @keyup.enter="addTask">
+    </label>
+    <label class="width block">Due
+      <input type="date" v-model="newTask.dueDate">
+    </label>
+    <div class="inline pad block">
+      <label class="pad">Priority</label>
+      <select v-model="newTask.priority">
+        <option value="high">high</option>
+        <option value="med">med</option>
+        <option value="low">low</option>
+      </select>
+    </div>
+    <div class="inline pad block">
+      <label>Category</label>
+      <select v-model="newTask.category">
+        <option value=""></option>
+        <option value="school">school</option>
+        <option value="work">work</option>
+        <option value="home">home</option>
+      </select>
+    </div>
+    <button @click="addTask">Add Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTask: {}
+    }
+  },
+
+  created () {
+    this.newTask = this.getNewTask()
+  },
+
+  methods: {
+    addTask () {
+      // this.taskList.push(this.newTask)
+      this.$emit('taskAdded', this.newTask)
+      this.newTask = this.getNewTask()
+    },
+
+    getNewTask () {
+      const now = new Date()
+      const today = now.toISOString().substr(0, now.toISOString().indexOf('T'))
+      console.log(now)
+      return {
+        id: null,
+        category: 'home',
+        dueDate: today,
+        isComplete: false,
+        priority: 'med',
+        title: ''
+      }
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+  .pad {
+    padding-bottom: 1rem;
+  }
+
+  input,
+  select {
+    border: solid 2px #01cdd7;
+    border-radius: 5px;
+    font-size: .7rem;
+    padding: .3rem;
+  }
+
+  input {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    font-size: 1.25rem;
+    font-weight: bold;
+    color: #fff;
+    background-color: #01cdd7;
+    border: solid 2px #01cdd7;
+    border-radius: 5px;
+    padding: .8rem;
+  }
+
+  label {
+      font-size: 1rem;
+      padding-right: 1rem;
+  }
+
+  .inline {
+    display: inline-block;
+  }
+
+  .block {
+   display: block;
+  }
+
+  button {
+    width: 100%;
+    margin-bottom: 1rem;
+    font-size: 1rem;
+  }
+
+
+@media only screen and (min-width: 38rem) {
+
+  .block {
+   display: inline-block;
+  }
+
+  button {
+    width: 100%;
+  }
+
+  input,
+  select {
+    border: solid 2px #01cdd7;
+    border-radius: 5px;
+    font-size: .7rem;
+    padding: .3rem;
+  }
+
+}
+
+</style>
