34a8130c94b485796746e76d2f13820afbf008c5
Hannah Hatherell <hath0013@algonquinlive.com>
2018-04-23 14:28:44 -0400

    added and updated login page
    added login page structure

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -61,13 +61,63 @@ export default {
   },
   methods: {
     login () {
+      this.errorMessage = ''
+      this.isWorking = true
+      axios.post(
+        'https://vue-todos.robertmckenney.ca/oauth/token',
+        {
+          grant_type: 'password',
+          client_id: '2',
+          client_secret: 'TJJiU0ULORMxmpToXaTX4J4qo1cttVJ3nGSkg1xk',
+          username: this.loginName,
+          password: this.password,
+          scope: '*'
+        }
+      )
+      .then(response => {
+        // eslint-disable-next-line
+        const { expires_in, ...rest } = response.data
+        const apiTokens = {
+          expires_at: moment().add(expires_in, 'seconds').format(),
+          ...rest
+        }
+        this.$emit('saveApiTokens', apiTokens)
+        this.loginName = null
+        this.password = null
+      })
+      .catch(error => this.handleError(error))
+      .finally(() => { this.isWorking = false })
+
+
       // send credentials to the OAuth2 server
       // get back an access_token
       // save the token
       // redirect to the main application view
     },
-    handleError () {
-      // we will create this later
+    handleError (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        switch (error.response.status) {
+          case 400: {
+            this.errorMessage = 'Both login name and password are required.'
+            break
+          }
+          case 401: {
+            this.errorMessage = 'Incorrect username or password'
+            break
+          }
+          default:
+            this.errorMessage = 'Sorry. There was an error processing your request.'
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request)
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message)
+      }
+      console.log(error.config)
     }
   }
 }
